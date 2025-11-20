33e0466723d3ee5dfa5d2cbea30a9d436dc7d0af
Nicolandya <nicilasa8@gmail.com>
2025-11-20 01:49:29 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot online como {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+bot.run(TOKEN)
